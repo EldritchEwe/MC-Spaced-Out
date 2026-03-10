29dd957d6b67e5fe40fe2f43447b81666247db19
EldritchEwe <eldritchewe@gmail.com>
2026-03-10 19:02:17 +1000

    General fixes

--- src/main/java/EldritchEwe/spacedOut/items/SO_ItemGroups.java
+++ src/main/java/EldritchEwe/spacedOut/items/SO_ItemGroups.java
@@ -12,26 +12,26 @@ import net.minecraft.util.Identifier;
 
 public class SO_ItemGroups
 {
-    public ItemGroup DESOLATEDUNES_TAB = Registry.register ( Registries.ITEM_GROUP,
+    public static final ItemGroup DESOLATEDUNES_TAB = Registry.register ( Registries.ITEM_GROUP,
         Identifier.of(SpacedOut.MOD_ID, "dunes"),
         FabricItemGroup.builder().icon(() -> new ItemStack(SO_Items.COARSE_DUST_CLUMP))
             .displayName(Text.translatable("itemGroup.spacedout.dunes"))
             .entries((displayContext, entries) ->
             {
-                entries.add ( SO_Blocks. COARSE_DUST.asItem()    );
-                entries.add ( SO_Items.  COARSE_DUST_CLUMP       );
-                entries.add ( SO_Blocks. SPECKLED_DUST.asItem()  );
-                entries.add ( SO_Items.  SPECKLED_DUST_CLUMP     );
-                entries.add ( SO_Blocks. SILVERY_DUST.asItem()   );
-                entries.add ( SO_Items.  SILVERY_DUST_CLUMP      );
-                entries.add ( SO_Blocks. COMPACTED_DUST.asItem() );
-                entries.add ( SO_Items.  COMPACTED_DUST_CLUMP    );
+                entries.add ( SO_Blocks. COARSE_DUST          );
+                entries.add ( SO_Items.  COARSE_DUST_CLUMP    );
+                entries.add ( SO_Blocks. SPECKLED_DUST        );
+                entries.add ( SO_Items.  SPECKLED_DUST_CLUMP  );
+                entries.add ( SO_Blocks. SILVERY_DUST         );
+                entries.add ( SO_Items.  SILVERY_DUST_CLUMP   );
+                entries.add ( SO_Blocks. COMPACTED_DUST       );
+                entries.add ( SO_Items.  COMPACTED_DUST_CLUMP );
 
-                entries.add ( SO_Blocks. ANCIENT_RUBBLE.asItem() );
-                entries.add ( SO_Items.  ANCIENT_GEARS           );
-                entries.add ( SO_Items.  ANCIENT_SHEET_METAL     );
-                entries.add ( SO_Items.  ANCIENT_RATION_PACK     );
-                entries.add ( SO_Items.  ANCIENT_WATER_PACK      );
+                entries.add ( SO_Blocks. ANCIENT_RUBBLE       );
+                entries.add ( SO_Items.  ANCIENT_GEARS        );
+                entries.add ( SO_Items.  ANCIENT_SHEET_METAL  );
+                entries.add ( SO_Items.  ANCIENT_RATION_PACK  );
+                entries.add ( SO_Items.  ANCIENT_WATER_PACK   );
     }).build());
 
     public static void registerItemGroups() {
